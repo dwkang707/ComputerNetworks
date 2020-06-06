f27bb65c4ad0b041d152a9865bc3a4a1d61cdb50
dwkang707 <dwkang707@gmail.com>
2020-06-07 04:16:47 +0900

    test 10

--- server.c
+++ server.c
@@ -167,7 +167,7 @@ int main(int argc, char *argv[])
             // strncmp(A, B, size_t n): A와 B 문자열을 n 만큼 비교 -> A가 길면 0보다 큰 값, B가 길면 0보다 작은 값, 같으면 0 return
             if (!strncmp(buffer, "GET /index.html", 15)) {
                 fd = open("index.html", O_RDONLY);
-                write(newsockfd, buffer, 20);
+                write(newsockfd, "index.html", 10);
                 //sendfile(newsockfd, fd, NULL, 1000);
                 close(fd);
             }
